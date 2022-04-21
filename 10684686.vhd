8458092a7ea21a1f134bee137ed84a3df9183b22
Lorenzo Migliari <lorenzo1.migliari@mail.polimi.it>
2022-04-21 18:28:21 +0200

    Aggiunta VHDL e constraints file

---
+++ 10684686.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/21/2022 06:16:53 PM
+-- Design Name: 
+-- Module Name: progetto_reti_logiche - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity progetto_reti_logiche is
+--  Port ( );
+end progetto_reti_logiche;
+
+architecture Behavioral of progetto_reti_logiche is
+
+begin
+
+
+end Behavioral;

0d0d79dba81ac5b3c9cc71a3f59afa89bfaa5ae2
Lorenzo Migliari <lorenzo1.migliari@mail.polimi.it>
2022-04-29 13:08:44 +0200

    Some Fix

--- 10684686.vhd
+++ 10684686.vhd
@@ -45,7 +45,6 @@ architecture Behavioral of progetto_reti_logiche is
           sel_increseMemAddress : in STD_LOGIC;
           sel_AddressOutput : in STD_LOGIC;
           sel_DataOutput : in STD_LOGIC;
-          startConvolution : in STD_LOGIC;
           o_data            : out  STD_LOGIC_VECTOR(7 downto 0);
           o_address         : out STD_LOGIC_VECTOR(15 downto 0);
           o_endWord      : out STD_LOGIC;
@@ -63,10 +62,9 @@ signal sel_decreaseCounter : STD_LOGIC;
 signal sel_increseMemAddress : STD_LOGIC;
 signal sel_AddressOutput : STD_LOGIC;
 signal sel_DataOutput : STD_LOGIC;
-signal startConvolution : STD_LOGIC;
 signal o_endWord : STD_LOGIC;
 signal o_endFile : STD_LOGIC;
-type S is (S0, Reset, InitLoad, Load, InitConvolution, Convolute, ConvoluteCheck, SaveP1, SaveP2, CloseMem);
+type S is (S0, Reset, InitLoad, WaitMem1, Load, WaitMem2, InitConvolution, Convolute, ConvoluteCheck, SaveP1, SaveP2, CloseMem);
 signal cur_state, next_state : S;
 
 begin
@@ -84,7 +82,6 @@ begin
           sel_increseMemAddress => sel_increseMemAddress ,
           sel_AddressOutput => sel_AddressOutput , 
           sel_DataOutput => sel_DataOutput ,
-          startConvolution => startConvolution ,
           o_data            => o_data ,
           o_address        => o_address ,
           o_endWord      => o_endWord ,
@@ -115,17 +112,17 @@ begin
         when Reset => --stato di Reset, passa immediatamente allo stato di InitLoad
           next_state <= InitLoad;
         when InitLoad => --stato di InitLoad, se il file non e terminato passa allo stato di Load, altrimenti torna a S0
-          if o_endFile = '0' then
+            next_state <= WaitMem1;
+        when WaitMem1 => --stato di Wait, permette di caricare il valore corretto dalla memoria
             next_state <= Load;
-          else
-            next_state <= S0;
-          end if;
         when Load => --stato di Load, se il file non e terminato passa alla fase di elaborazione
           if(o_endFile = '0') then
-            next_state <= InitConvolution;
+            next_state <= WaitMem2 ;
           else
             next_state <= CloseMem;
           end if;
+        when WaitMem2   => --stato di Wait, permette di caricare il valore corretto dalla memoria
+            next_state <= InitConvolution;
         when InitConvolution => --stato per iniziare la convoluzione, passa subito allo stato successivo
           next_state <= Convolute;
         when Convolute => --stato per eseguire la convoluzione di una data parola
@@ -138,7 +135,7 @@ begin
             end if; 
          when SaveP1 => --stato per salvare in memoria il risultato dello Convolute 
             next_state <= SaveP2 ;
-         when SaveP2 => 
+         when SaveP2 =>  --stato per salvare in memoria il risultato dello Convolute 
             next_state <= Load ;
          when CloseMem => --stato finale, setta a zero l'accesso alla memoria
             next_state <= S0;
@@ -159,13 +156,13 @@ begin
       sel_increaseAddress <= '0';
       sel_decreaseCounter <= '0';
       sel_increseMemAddress <= '0';
-      startConvolution <= '0';
       o_we <= '0';
       o_en <= '0';
       --gestione del comportamento per ogni stato
       case cur_state is
         when s0 =>  --stato di IDLE
-        when Reset =>  --stato di reset, azzera il contatore
+        o_done <= '0';
+        when Reset =>  --stato di reset, azzera i contatori e gli indirizzi
           o_en <= '0';
           o_we <= '0';
           sel_increaseAddress <= '0';
@@ -175,34 +172,34 @@ begin
           rCounter_load <= '1';
           sel_increseMemAddress <= '0';
           rMemAddress_load <= '1';
-          startConvolution  <= '0';
         when InitLoad =>  --stato iniziale, carica il numero di parole da leggere, e si prepara a leggere la prima parola
           o_en <= '1';
           o_we <= '0';
-          rmaxAddress_load <= '1';
           sel_increaseAddress <= '1';
-          rAddress_load <= '1';
+          sel_AddressOutput <= '0';
           rCounter_load <= '0';
+          rAddress_load <= '0';
           rMemAddress_load <= '0';
+        when WaitMem1 => --aspetta che la parola sia effettivamente caricata
+          rmaxAddress_load <= '1';
         when Load => --stato di Load, carica la parola corrente, si prepara a leggere la successiva
           o_en <= '1';
           o_we <= '0';
-          rstream_load <= '1';
           sel_increaseAddress <= '1';
           rAddress_load <= '1';
-          startConvolution  <= '0';
           rMemAddress_load <= '0';
           sel_AddressOutput <= '0';
           sel_decreaseCounter <= '0';
           rCounter_load <= '1';
-        when InitConvolution =>
+        when WaitMem2 => --aspetta che la parola sia effettivamente caricata
+          rstream_load <= '1';
+        when InitConvolution => --setta i segnali per iniziare la convoluzione
           o_en <= '0';
           o_we <= '0';
           rstream_load <= '0';
           rAddress_load <= '0';
           sel_decreaseCounter <= '1';
           rCounter_load <= '1';
-          startConvolution  <= '1';
         when Convolute => 
           rCounter_load <= '0';
         when ConvoluteCheck =>
@@ -213,7 +210,6 @@ begin
            o_we <= '1'; 
            sel_decreaseCounter <= '0';
            rCounter_load <= '0';
-           startConvolution  <= '0';
            sel_increseMemAddress <= '1';
            rMemAddress_load <= '1';
            sel_AddressOutput  <= '1';
@@ -228,6 +224,7 @@ begin
          when CloseMem => 
            o_en <= '0';
            o_we <= '0'; 
+           o_done <= '1';
       end case;
     end process;
 
@@ -255,7 +252,6 @@ entity datapath is
           sel_increseMemAddress : in STD_LOGIC;
           sel_AddressOutput : in STD_LOGIC;
           sel_DataOutput : in STD_LOGIC;
-          startConvolution : in STD_LOGIC;
           o_data            : out  STD_LOGIC_VECTOR(7 downto 0);
           o_address         : out STD_LOGIC_VECTOR(15 downto 0);
           o_endWord      : out STD_LOGIC;
@@ -265,7 +261,7 @@ end datapath;
 architecture Behavioral of datapath is
   signal o_rStream : STD_LOGIC_VECTOR(7 downto 0);
   signal data_sum : STD_LOGIC_VECTOR(15 downto 0);
-  signal o_rMaxAddress : STD_LOGIC_VECTOR(15 downto 0);
+  signal o_rMaxAddress : STD_LOGIC_VECTOR(7 downto 0);
   signal mux_rAddress : STD_LOGIC_VECTOR(15 downto 0);
   signal o_rAddress : STD_LOGIC_VECTOR(15 downto 0);
   signal MaxAddress_sum : STD_LOGIC_VECTOR(15 downto 0);
@@ -281,6 +277,7 @@ architecture Behavioral of datapath is
   signal mux_rCounter : integer ;
   signal rCounter_sub : integer ;
   signal tempData : integer ;
+  signal tempAddress : integer;
 begin
 
 --configurazione del registro Stream
@@ -296,9 +293,10 @@ begin
   end process;
 
   --configurazione MaxAddressSum, aumenta di 1 il MaxAddress
-  tempData <= (to_integer(unsigned(i_data)) + 1);
-  MaxAddress_sum <= std_logic_vector(to_unsigned(tempData, MaxAddress_sum 'length));
+    tempData <= (to_integer(unsigned(o_rMaxAddress)) + 1);
+    MaxAddress_sum <= std_logic_vector(to_unsigned(tempData, MaxAddress_sum 'length));
 
+    
   --configurazione del registro MaxAddress
   process(i_clk, i_rst)
   begin
@@ -306,16 +304,20 @@ begin
       o_rMaxAddress <= (others => '0');
     elsif i_clk'event and i_clk = '1' then
       if(rMaxAddress_load = '1') then
-        o_rMaxAddress <= MaxAddress_sum; 
+        o_rMaxAddress <= i_data; 
       end if;
     end if;
   end process;
+  
+  --configurazione del nodo sommatore address_sum 
+  tempAddress  <= (to_integer(unsigned(o_rAddress)) + 1);
+  address_sum <= std_logic_vector(to_unsigned(tempAddress, address_sum 'length));
 
   --configurazione mux per l'aumento o il reset del contatore Address
   with sel_increaseAddress select  mux_rAddress <=   
     (others => '0') when '0',
     address_sum  when '1',
-    (others => 'X') when others;
+    (others => '0') when others;
 
   --configurazione registro Address, che contiene l'indirizzo da leggere in memoria
   process(i_clk, i_rst)
@@ -330,7 +332,7 @@ begin
   end process;
     
   --configurazione endFIle_sub, si occupa di confrontare il MaxAddress con líndirizzo corrrente (o_raddress) aumentato di 1 => address_sum
-  endFile_sub <= std_logic_vector(unsigned(o_rMaxAddress (15 downto 0)) - unsigned(address_sum(15 downto 0)));
+  endFile_sub <= std_logic_vector(unsigned(MaxAddress_sum) - unsigned(address_sum));
   o_endFile  <=  '1' when (endFile_sub = "0000000000000000" ) else '0';
     
   --configurazione registro Counter, che contiene il numero di bit letti
@@ -363,7 +365,6 @@ begin
       P1 <= (others => '0');
       P2 <= (others => '0');
     elsif i_clk'event and i_clk = '1' then
---        if (startConvolution = '1') then
              case o_rCounter  is
                 when 7 =>
                     P1 (o_rCounter ) <= (o_rStream(o_rCounter) xor '0');
@@ -376,8 +377,7 @@ begin
                     P2 (o_rCounter ) <= (o_rStream(o_rCounter) xor o_rStream(o_rCounter + 1) xor o_rStream(o_rCounter + 2));
                  when others => 
               end case;
---         end if;
-    end if;
+         end if;
   end process;
  
   --configurazione registro memAddress, che contiene l'indirizzo in cui scrivere in memoria e si occupa di fare output dei dati
@@ -398,7 +398,7 @@ begin
   with sel_increseMemAddress select mux_rMemAddress <=   
     std_logic_vector(to_unsigned(1000, mux_rMemAddress'length)) when '0',
     MemAddress_sum  when '1',
-    (others => 'X') when others;
+    (others => '0') when others;
     
     --configurazione mux per output address 
     with sel_AddressOutput select o_address <=
